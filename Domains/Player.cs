72df4a9e75cca28f8d6afd1bb468170b6dcf18c2
kirillchertok <kirill250305@gmail.com>
2024-12-10 12:51:05 +0300

    Finish domains

---
+++ Domains/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domains
+{
+	public class Player
+	{
+		public string Username { get; set; }
+		public int Points { get; set; } = 0;
+		public List<Card>? Cards { get; set; }
+		public int ValueOfCards { get; set; } = 0;
+		public string LastAction { get; set; } = "Connected";
+
+		public Player() { }
+
+		public Player(string username, string lastAction)
+		{
+			this.Username = username;
+			this.LastAction = lastAction;
+		}
+	}
+}
